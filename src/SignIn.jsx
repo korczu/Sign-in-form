45e15443134c60f8b416c046380e35854079956c
akorczynska <korczynaa@gmail.com>
2022-12-23 09:44:49 +0100

    created sign in form

---
+++ src/SignIn.jsx
@@ -0,0 +1,83 @@
+import React from "react";
+import {
+  Box,
+  Button,
+  Checkbox,
+  FormControlLabel,
+  Link,
+  TextField,
+  Typography,
+} from "@mui/material";
+import LockOpenIcon from "@mui/icons-material/LockOpen";
+
+function SignIn({ onSubmit, handleRememberMe }) {
+  function handleSubmit(e) {
+    e.preventDefault();
+    if (typeof onSubmit === "function") return onSubmit();
+    throw new Error("onSubmit isn't function");
+  }
+  return (
+    <Box
+      sx={{
+        height: "100vh",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        flexDirection: "column",
+        wordBreak: "break-all",
+      }}
+    >
+      <LockOpenIcon />
+      <Typography component="h1" variant="h4">
+        Sign in
+      </Typography>
+      <Box
+        component="form"
+        onSubmit={handleSubmit}
+        noValidate
+        sx={{
+          display: "flex",
+          flexDirection: "column",
+        }}
+      >
+        <TextField
+          margin="normal"
+          id="login"
+          label="Login"
+          name="login"
+          variant="outlined"
+          autoFocus
+          required
+        />
+        <TextField
+          margin="normal"
+          id="password"
+          label="Password"
+          type="password"
+          name="password"
+          variant="outlined"
+          autoFocus
+          required
+        />
+        <FormControlLabel
+          control={<Checkbox />}
+          label="Remember me"
+          onChange={handleRememberMe}
+        />
+        <Button type="submit" variant="contained">
+          submit
+        </Button>
+        <Box sx={{ display: "flex", flexDirection: "column" }}>
+          <Link underline="none" href="#">
+            Reset password
+          </Link>
+          <Link underline="none" href="#">
+            Create account
+          </Link>
+        </Box>
+      </Box>
+    </Box>
+  );
+}
+
+export default SignIn;
